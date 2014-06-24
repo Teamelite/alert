290aff867cd04aeea38db084c7263c323be0e5c6
dave bentley <david.bentley94@btinternet.com>
2014-06-24 07:29:46 +0100

    sleepy

--- src/alert/Alert.java
+++ src/alert/Alert.java
@@ -15,7 +15,8 @@ public class Alert extends JavaPlugin {
         PluginManager pm = this.getServer().getPluginManager();
         pm.registerEvents(new BlockPlace(), this);
         pm.registerEvents(new Interact(), this);
-
+        pm.registerEvents(new ChestCount(), this);
+                
         getCommand("setworld").setExecutor(new Commands());
         getCommand("showworld").setExecutor(new Commands());
         getCommand("countchests").setExecutor(new Commands());
